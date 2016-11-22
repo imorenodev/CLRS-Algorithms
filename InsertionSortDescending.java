63519d355c7580d75ed1c7edc6d057a101b61c33
imorenodev@gmail.com <imorenodev@gmail.com>
2016-11-22 09:03:56 -0500

    Insertion Sort Algorithm implemented to sort array in descending order

---
+++ InsertionSortDescending.java
@@ -0,0 +1,35 @@
+public class InsertionSortDescending {
+  public static void main(String[] args) {
+    int[] array = {5,2,4,6,1,3};
+    int i = 0,
+        j = 0,
+        key = 0;
+
+    // print unsorted array
+    printArray(array);
+
+    for (j = 0; j < array.length; j++) {
+      key = array[j]; 
+      i = j-1; 
+      while (i >= 0 && array[i] < key) {
+        array[i+1] = array[i];
+        i--;
+      }
+      array[i+1] = key;
+    }
+    
+    //print sorted array
+    printArray(array);
+  }
+
+  public static void printArray(int[] array) {
+    int i = 0;
+    for (i = 0; i < array.length; i++) {
+      if (i == array.length-1) {
+        System.out.print(array[i] + "\n");
+      } else {
+        System.out.print(array[i] + ", ");
+      }
+    }
+  }
+}
